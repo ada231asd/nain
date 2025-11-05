fd03c15672663cae67fb3fe06ee1aa4500483111
KRUFAVAR <kruglovm108@gmail.com>
2025-11-05 17:08:13 +0300

    Full screen

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig(({ mode }) => {
           theme_color: '#ffffff',
           background_color: '#ffffff',
           display: 'standalone',
+          display_override: ['standalone', 'fullscreen'],
           scope: '/',
           start_url: '/',
           orientation: 'portrait',
